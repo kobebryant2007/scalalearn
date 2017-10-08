d290edce6afba65a827e8cb854d1da69a29f3960
kobebryant2007 <kobebryant2007@163.com>
2017-10-08 15:47:52 +0800

    添加list

--- src/com/ln/chapter1/HelloWorld.scala
+++ src/com/ln/chapter1/HelloWorld.scala
@@ -3,5 +3,32 @@ package com.ln.chapter1
 object HelloWorld {
   def main(args: Array[String])= {
     println("hello scala world");
+    println(formatAbs(-6));
+    println(factorial(16))
+  }
+  def abs(n: Int):Int={
+    if(n < 0) -n
+    else n
+  }
+  
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x));
+  }
+  
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int = 
+      if(n <= 0) acc
+      else go(n-1, n * acc)
+     
+    go(n, 1)
+  }
+  
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    def loop(n: Int): Int = 
+      if(n >= as.length) -1
+      else if(p(as(n))) n
+      else loop(n + 1)
+    loop(0)
   }
 }
---
+++ src/com/ln/chapter1/List.scala
@@ -0,0 +1,5 @@
+package com.ln.chapter1
+
+object List {
+  
+}
